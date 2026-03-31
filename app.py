5fddca62ff71437db93bfd198234bf0e31e47ef1
rakeshyadav1289 <129735141+rakeshyadav1289@users.noreply.github.com>
2026-03-31 20:21:56 +0530

    Fix typo in home route response message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello Rakesh! Flask running inside Docker . This webhook upated ..........................</h1>🚀"
+    return "<h1>Hello Rakesh! Flask running inside Docker  . This webhook upated ..........................</h1>🚀"
 
 @app.route("/health")
 def health():
